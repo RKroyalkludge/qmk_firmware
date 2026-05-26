517bd17ce0e1b1a1835b78954b6ad388df8e506a
sdk <2371260634@qq.com>
2026-05-26 16:38:15 +0800

    update

--- keyboards/ilovbee/b87_usb/post_rules.mk
+++
@@ -1,5 +0,0 @@
-RGB_MATRIX_CUSTOM_USER = yes
-RGBLIGHT_DRIVER = custom
-include keyboards/ilovbee/b87_usb/rgb_record/rgb_record.mk
-include keyboards/ilovbee/b87_usb/wls/wls.mk
-include keyboards/ilovbee/linker/wireless/wireless.mk
--- keyboards/ilovbee/b98_us_triple_mode/post_rules.mk
+++
@@ -1,5 +0,0 @@
-RGB_MATRIX_CUSTOM_USER = yes
-RGBLIGHT_DRIVER = custom
-include keyboards/ilovbee/b98_us_triple_mode/rgb_record/rgb_record.mk
-include keyboards/ilovbee/b98_us_triple_mode/wls/wls.mk
-include keyboards/ilovbee/linker/wireless/wireless.mk
